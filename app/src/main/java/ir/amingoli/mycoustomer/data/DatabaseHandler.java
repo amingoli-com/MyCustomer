a613c92838961a682a0794c3fc429516eeca5e5e
amingoli78 <amin.goli78@gmail.com>
2020-10-17 01:28:50 +0330

    show list customer order by date DESC

--- app/src/main/java/ir/amingoli/mycoustomer/data/DatabaseHandler.java
+++ app/src/main/java/ir/amingoli/mycoustomer/data/DatabaseHandler.java
@@ -416,7 +416,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         Cursor cursor = db.query(TABLE_ORDER,
                 new String[] {},
                 COL_ORDER_STATUS + " IN ("+status+")"+ " AND " +COL_ORDER_ID_CUSTOMER + " IN ("+idCustomer+")",
-                null, null, null, null);
+                null, null, null, COL_ORDER_CREATED_AT + " DESC");
 
         if (cursor.moveToFirst()) {
             items = getListOrderByCursor(cursor);
